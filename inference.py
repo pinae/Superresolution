b15ede3ce900c8a9bc4ccab302b48b6b48e3105c
pinae <jonny@pinae.net>
2017-04-10 13:12:46 +0200

    Code for inferencing

--- inference.py
+++ inference.py
@@ -9,6 +9,6 @@ import os
 if __name__ == "__main__":
     image_size = (320, 240)
     net = Network(dimensions=(320, 240), batch_size=1)
-    im = size(Image.open(os.path.join("images", "pexels-photo-25953.jpg")), (image_size[0]*2, image_size[1]*2))
-    net.train_step(images=[np.array(im)])
-    #Image.fromarray(inference_image_data)
+    im = size(Image.open(os.path.join("images", "pexels-photo-25953.jpg")), (image_size[0], image_size[1]))
+    output = net.inference(images=[np.array(im)])
+    Image.fromarray(output[0].astype(np.uint8)).show()
